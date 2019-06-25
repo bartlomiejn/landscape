c531d56d10f9638f4e38481a60014ad38e748931
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2019-06-25 19:39:48 +0200

    Added GLFW window setup, basic keyboard handling and viewport setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,51 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#define WIN_RES_X 800
+#define WIN_RES_Y 600
+
+void on_framebuffer_resize(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+	GLFWwindow* window = glfwCreateWindow(
+		WIN_RES_X, WIN_RES_Y, "Landscape Engine", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD." << std::endl;
+		return -1;
+	}
+	glfwSetFramebufferSizeCallback(window, on_framebuffer_resize);
+	glViewport(0, 0, WIN_RES_X, WIN_RES_Y);
+	while(!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return 0;
 }
