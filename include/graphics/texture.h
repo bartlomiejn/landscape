eacb7b5bb13de544eba956bc848073842551508d
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2019-07-08 21:46:42 +0200

    Added data buffer Texture initializer

--- include/graphics/texture.h
+++ include/graphics/texture.h
@@ -3,22 +3,36 @@
 
 #include <glad/glad.h>
 #include <graphics/image.h>
+#include <cstddef>
 
 enum ColorLayout
 {
+	layout_r,
 	layout_rgb,
-	layout_rgba
+	layout_rgba,
 };
 
 class Texture
 {
 public:
-	/// Generates an OpenGL texture from provided image.
+	/// Generates an OpenGL texture from provided image. `load` the
+	/// texture before use!
 	///
 	/// \param image Image to create a texture from. Should be loaded before
 	/// use.
 	/// \param layout Color layout to use.
 	Texture(Image& image, ColorLayout layout);
+	
+	/// Generate an OpenGL texture from provided image data. `load` the
+	/// texture before use!
+	///
+	/// \param data Image buffer to create a texture from.
+	/// \param width Buffer width in bytes.
+	/// \param height Buffer height in bytes.
+	/// \param layout Color layout to use.
+	Texture(
+		unsigned char *data, unsigned int width, unsigned int height,
+		ColorLayout layout);
 	
 	/// Loads the texture.
 	void load();
@@ -29,7 +43,9 @@ public:
 	void use(GLenum tex_unit);
 private:
 	unsigned int id;
-	Image& image;
+	const unsigned char *data;
+	const int width;
+	const int height;
 	ColorLayout layout;
 };
 
--- src/graphics/texture.cpp
+++ src/graphics/texture.cpp
@@ -1,8 +1,20 @@
 #include <graphics/texture.h>
 
 Texture::Texture(Image& tex_image, ColorLayout tex_layout):
-	image(tex_image),
+	data(tex_image.data()),
+	width(tex_image.width()),
+	height(tex_image.height()),
 	layout(tex_layout)
+{}
+
+Texture::Texture(
+	unsigned char *data, unsigned int width, unsigned int height,
+	ColorLayout layout
+):
+	data(data),
+	width(width),
+	height(height),
+	layout(layout)
 {}
 
 void
@@ -11,6 +23,9 @@ Texture::load()
 	GLenum format;
 	switch (layout)
 	{
+		case layout_r:
+			format = GL_RED;
+			break;
 		case layout_rgb:
 			format = GL_RGB;
 			break;
@@ -27,8 +42,8 @@ Texture::load()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexImage2D(
-		GL_TEXTURE_2D, 0, format, image.width(), image.height(), 0,
-		format, GL_UNSIGNED_BYTE, image.data());
+		GL_TEXTURE_2D, 0, format, width, height, 0, format,
+		GL_UNSIGNED_BYTE, data);
 	glGenerateMipmap(GL_TEXTURE_2D);
 }
 
